99189d8aee0ef9f1aac961587f3517717aea1dc5
swapnil-shah <swapnilshah.me@gmail.com>
2021-02-10 11:51:14 -0500

    Fetching individual data with useEffect

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -1,24 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-
+// Controlled Component: we need to set the value and listen to the unchanged event to assign the value back to the input element
 function DataFetching() {
-  const [posts, setPosts] = useState([])
+  const [post, setPost] = useState([])
+  const [id, setId] = useState(1)
 
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then(res => {
         console.log(res)
-        setPosts(res.data)
+        setPost(res.data)
       })
       .catch(err => {
         console.log(err)
       })
-  }, [])
+  }, [id])
   return (
     <div>
-      <ul>
-        {posts.map(post => <li key={post.id}>{post.title}</li>)}
-      </ul>
+      <input
+        type="text"
+        value={id}
+        onChange={e => setId(e.target.value)}
+      />
+
+      {/* <ul>
+        {post.map(post => <li key={post.id}>{post.title}</li>)}
+      </ul> */}
+      <div>
+        {post.title}
+      </div>
     </div>
   )
 }
